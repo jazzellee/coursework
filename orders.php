dc32ccf55f353a63d0e4d76469c1572cde866df4
jazzellee <lee.y@oundleschool.org.uk>
2025-09-19 15:47:42 +0100

    kms

--- orders.php
+++ orders.php
@@ -11,17 +11,18 @@
     Date Ordered:<input type="date" name="date"><br>
     <br>
     <!--radio buttons-->
-    <input type="radio" name="paid" value="User" checked> User<br>
-    <input type="radio" name="paid" value="Admin"> Admin<br>
-    <input type="submit" value="Add User">
+    <input type="radio" name="paid" value="Unpaid" checked> Unpaid<br>
+    <input type="radio" name="paid" value="Paid"> Paid<br>
+    <br>
 
-    <input type="radio" name="status" value="User" checked> User<br>
-    <input type="radio" name="status" value="Admin"> Admin<br>
-    <input type="radio" name="status" value="Admin"> Admin<br>
-    <input type="submit" value="Add User">
+    <input type="radio" name="status" value="Preparing" checked> Preparing<br>
+    <input type="radio" name="status" value="Dispatched "> Dispatched<br>
+    <input type="radio" name="status" value="Out For Delivery"> Out for Delivery<br>
+    <input type="radio" name="status" value="Delivered"> Delivered<br>
+    <input type="submit" value="Submit">
 
     </form>
-    <h2>Current users</h2>
+    <h2>Your Orders</h2>
     <?php
     include_once("connection.php");
     $stmt = $conn->prepare("SELECT * FROM tblusers");
