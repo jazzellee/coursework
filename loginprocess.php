38cf81f5f3fa778adce91e495a634267df63c20c
jazzellee <jl4100140@gmailcom>
2025-09-19 20:27:34 +0100

    errors

--- loginprocess.php
+++ loginprocess.php
@@ -1,20 +1,17 @@
 <?php
 include_once ("connection.php");
 array_map("htmlspecialchars", $_POST);
-$stmt = $conn->prepare("SELECT * FROM tblusers WHERE Email = email ;" );
+$stmt = $conn->prepare("SELECT * FROM tblusers WHERE Email = :email ;" );
 $stmt->bindParam(':username', $_POST['Username']);
 $stmt->execute();
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-{ 
-    
-    
-    $pwcomp = password_hash($_POST["passwd"], PASSWORD_BCRYPT) //password hash
-    
+{  
+    $pwcomp = password_hash($_POST["passwd"], PASSWORD_BCRYPT); //password hash 
     if($row['password']== $pw){
         if($row['role'] == 1){
             header('Location: admin.php');
         }else{
-            header('Location: users.php')
+            header('Location: users.php');
         }
     }else{
 
