b7b913118297d47b045057dc3af39364666f55b7
jazzellee <jl4100140@gmailcom>
2025-09-19 18:03:20 +0100

    merge

--- addorders.php
+++ addorders.php
@@ -2,44 +2,47 @@
 include_once("connection.php");
 
 if (
-    isset($_POST["userid"], $_POST["bookid"], $_POST["isbn"], $_POST["borrowdate"], $_POST["status"])
+    isset($_POST["orderid"], $_POST["userid"], $_POST["date"], $_POST["paid"], $_POST["status"])
 ) {
     array_map("htmlspecialchars", $_POST);
 
-print_r($_POST);
-$borrowdate = $_POST["borrowdate"]; // Assuming it's a valid date string
 
-// Create a DateTime object from the borrowdate
-$borrowdate_obj = new DateTime($borrowdate);
-
-// Add 14 days
-$borrowdate_obj->add(new DateInterval("P14D"));
-
-// Get the due date and format it to only show the date (Y-m-d)
-$duedate = $borrowdate_obj->format("Y-m-d");
-
-echo $duedate; // Display the due date
+    switch ($_POST["paid"]) {
+        case "Unpaid":
+            $paid = 0;
+            break;
+        case "Paid":
+            $paid = 1;
+            break;
+        default:
+            $paid = null;
+    }
 
     switch ($_POST["status"]) {
-        case "On Loan":
+        case "Preparing":
             $status = 0;
             break;
-        case "Returned":
+        case "Dispatched":
             $status = 1;
+            break;
+        case "Out for Delivery":
+            $status = 2;
+            break;
+        case "Delivered":
+            $status = 3;
             break;
         default:
             $status = null;
     }
     
+    
 
 
-    if ($status !== null) {
-
-       
-            $stmt = $conn->prepare("INSERT INTO tblloans(userid, bookid, isbn, borrowdate, duedate, status)
+    if ($paid !== null) {
+            $stmt = $conn->prepare("INSERT INTO tblorders(userid, bookid, isbn, borrowdate, duedate, status)
                 VALUES (:userid, :bookid, :isbn, :borrowdate, :duedate, :status)");
             
-        //try {
+        try {
             $stmt->bindParam(':userid', $_POST["userid"]);
             $stmt->bindParam(':bookid', $_POST["bookid"]);
 			$stmt->bindParam(':isbn', $_POST["isbn"]);
@@ -50,13 +53,13 @@ echo $duedate; // Display the due date
             $stmt->execute();
 
             
-         //   header('Location: users.php');
-         //   exit();
-       // } catch (PDOException $e) {
+            header('Location: users.php');
+            exit();
+        } catch (PDOException $e) {
             
-        //    error_log("Database error: " . $e->getMessage());
-         //   echo "An error occurred. Please try again later.";
-        //}
+            error_log("Database error: " . $e->getMessage());
+            echo "An error occurred. Please try again later.";
+        }
     
     } else {
         echo "Please select loan status.";
